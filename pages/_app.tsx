5a2de3cba425417faa204c761c8e3a772f589610
Cole Gawin <colegawin@gmail.com>
2021-11-21 15:01:38 -0600

    ✨: add plausible analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,20 +17,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta charSet={"utf-8"} />
-
-        <link rel={"icon"} href={"favicon.png"} />
-        <link rel={"manifest"} href={"/manifest.json"} />
-
-        <link rel={"apple-touch-icon"} href={"/icons/icon-192x192.png"}></link>
-        <meta name={"apple-mobile-web-app-status-bar-style"} content={"black"} />
-
-        <meta
-          name={"description"}
-          content={
-            "Will today be a bones day? Will it be a no bones day? Keep up to date with Noodle the Pug's (almost) daily forecasts for whether you should wear soft pants or treat yo'self!"
-          }
-        />
         <title>is it a bones day?</title>
       </Head>
       <AppWrapper>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,23 @@ export default class Document extends NextDocument {
     return (
       <Html lang={"en"}>
         <Head>
+          <meta charSet={"utf-8"} />
+
+          <link rel={"icon"} href={"favicon.png"} />
+          <link rel={"manifest"} href={"/manifest.json"} />
+
+          <link rel={"apple-touch-icon"} href={"/icons/icon-192x192.png"}></link>
+          <meta name={"apple-mobile-web-app-status-bar-style"} content={"black"} />
+
+          <meta
+            name={"description"}
+            content={
+              "Will today be a bones day? Will it be a no bones day? Keep up to date with Noodle the Pug's (almost) daily forecasts for whether you should wear soft pants or treat yo'self!"
+            }
+          />
+
           <script async defer src="https://www.tiktok.com/embed.js"></script>
+          <script defer data-domain="isitabonesday.app" src="https://plausible.io/js/plausible.js"></script>
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
