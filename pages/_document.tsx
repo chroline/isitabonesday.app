b0885c2a1d1a2be375efff64476bf6eff845a4ad
Cole Gawin <colegawin@gmail.com>
2021-11-21 21:26:28 -0600

    ✨: add adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,12 +23,31 @@ export default class Document extends NextDocument {
 
           <script async defer src="https://www.tiktok.com/embed.js"></script>
           <script defer data-domain="isitabonesday.app" src="https://plausible.io/js/plausible.js"></script>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9178014579594520"
+            crossOrigin="anonymous"
+          ></script>
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
           <Main />
           <NextScript />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9178014579594520"
+            crossOrigin="anonymous"
+          ></script>
+          <ins
+            className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-9178014579594520"
+            data-ad-slot="7592455572"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
         </body>
       </Html>
     );
